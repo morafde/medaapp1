356660305c0bdd7a3d32250b7e50a98368e06475
Sean Mo <morafde@yahoo.com>
2017-04-22 00:55:00 +0100

    First Commit

---
+++ config/routes.rb
@@ -0,0 +1,18 @@
+Rails.application.routes.draw do
+  resources :hospitals
+  get 'pages/Hospitals'
+
+  get 'pages/Labs'
+
+  get 'pages/Doctors'
+
+  get 'pages/about'
+
+  get 'pages/contact'
+  
+  root'hospitals#index'
+
+
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
