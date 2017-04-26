c53a432cfbb5768372a4e0bf3c1e3000f681bff4
Sean Mo <morafde@yahoo.com>
2017-04-26 12:14:00 +0100

    Fix upload issue finally

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,7 +3,11 @@ CarrierWave.configure do |config|
   config.fog_credentials = {
     provider:              'AWS',                        # required
     aws_access_key_id:     ENV["aws_access_key_id"],                        # required
-    aws_secret_access_key: ENV["aws_secret_access_key"],                        # required
+    aws_secret_access_key: ENV["aws_secret_access_key"], 
+    region: 'us-west-2', 
+    path_style: 'true'                       # required
   }
   config.fog_directory  = ENV["fog_directory"]                          # required
+  config.fog_public     = false                                        # optional, defaults to true
+  config.fog_attributes = { 'Cache-Control' => "max-age=#{365.day.to_i}" } # optional, defaults to {}
 end
