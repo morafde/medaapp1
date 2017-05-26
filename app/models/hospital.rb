89c6759e561902b6a394dec729f39cd50dc25a5e
Sean Mo <morafde@yahoo.com>
2017-05-26 14:55:58 +0100

    Add data validations

--- app/models/hospital.rb
+++ app/models/hospital.rb
@@ -2,4 +2,12 @@ class Hospital < ApplicationRecord
 	mount_uploader :image, ImageUploader
 
 	has_many :reviews
+
+	validates :name, :address, :phone, :website, :image, presence: true
+	validates :website, format: { with: /\Ahttps?:\/\/.*\z/,
+  	message: "must start with http:// or https://" }
+	validates :phone, numericality: {
+    only_integer: true,
+    message: "must be land line(7 digits) or Gsm(11 digits)"
+	}
 end
