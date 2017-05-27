7e9b60990bc59941c3b0fe5dfe0fe3626e97ea1f
Sean Mo <morafde@yahoo.com>
2017-05-27 08:17:16 +0100

    Remove phone validation

--- app/models/hospital.rb
+++ app/models/hospital.rb
@@ -6,8 +6,8 @@ class Hospital < ApplicationRecord
 	validates :name, :address, :phone, :website, :image, presence: true
 	validates :website, format: { with: /\Ahttps?:\/\/.*\z/,
   	message: "must start with http:// or https://" }
-	validates :phone, numericality: {
-    only_integer: true,
-    message: "must be land line(7 digits) or Gsm(11 digits)"
-	}
+	#validates :phone, numericality: {
+    #only_integer: true,
+    #message: "must be land line(7 digits) or Gsm(11 digits)"
+	#}
 end
